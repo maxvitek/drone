afe69bc240b74e4e885a059964679a3d5636504d
Max Vitek <maxvitek@gmail.com>
2014-03-21 13:34:28 -0500

    setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author='maxvitek',
     author_email='',
     description='',
-    install_requires=['logging_subprocess', 'unittest2'],
+    install_requires=['logging_subprocess', 'unittest2', 'logutils'],
     entry_points={
         'console_scripts':
             ['drone = drone:main', ]
