4605927961e259ed97961f93591d17a83bc47a82
Max Vitek <maxvitek@gmail.com>
2014-03-21 13:30:18 -0500

    setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='drone',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/maxvitek/drone',
+    license='',
+    author='maxvitek',
+    author_email='',
+    description='',
+    entry_points={
+        'console_scripts':
+            ['drone = drone:main', ]
+    }
+)
